5c16a630e12f48bd769078394afee966f06ec8f9
gabuso <gabuso@gabuso.com>
2013-09-25 11:00:53 +0200

    Removed unused libraries

--- filters/filter_lib.js
+++ filters/filter_lib.js
@@ -1,7 +1,3 @@
-var sylvester = require('sylvester');
-var Matrix = sylvester.Matrix;
-var Vector = sylvester.Vector;
-
 module.exports = Filter;
 var fact =  [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600];
 
